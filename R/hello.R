80312725cd1785fdc0ab3f004d4e60d27e4401db
FrankLef <flefebvre01@hotmail.com>
2024-07-27 14:46:22 -0400

    feat(normalz): remove

--- R/hello.R
+++ R/hello.R
@@ -11,7 +11,7 @@ hello <- function(msg = "Hello, world!") {
   if (grepl(pattern = "^hello.*", x = msg, ignore.case = TRUE)) {
     message(msg)
   } else {
-    msg_head <- cli::col_yellow("Message must start with \"hello\"")
+    msg_head <- cli::col_red("Message must start with \"hello\"")
     msg_body <- c("i" = sprintf("Message: %s", msg))
     msg <- paste(msg_head, rlang::format_error_bullets(msg_body), sep = "\n")
     rlang::abort(
